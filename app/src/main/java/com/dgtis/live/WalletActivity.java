c7b6999315a862c40cb401a8a014f1c83a2afd9d
Leeeg <03057411leeg>
2018-02-07 15:53:46 +0800

    个人主页 账户页面

---
+++ app/src/main/java/com/dgtis/live/WalletActivity.java
@@ -0,0 +1,28 @@
+package com.dgtis.live;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.dgtis.live.myapplication.R;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
+
+/**
+ * Created by DELLL on 2018/2/7.
+ */
+
+@ContentView(R.layout.activity_wallet)
+public class WalletActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Event(R.id.tab_top_back)
+    private void Close(View view){
+        finish();
+    }
+}
--- app/src/main/java/com/dgtis/live/fragments/PersonalFragment.java
+++ app/src/main/java/com/dgtis/live/fragments/PersonalFragment.java
@@ -1,15 +1,18 @@
 package com.dgtis.live.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
+import com.dgtis.live.WalletActivity;
 import com.dgtis.live.adapters.BaseFragmentAdapter;
 import com.dgtis.live.myapplication.R;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 
 import java.util.ArrayList;
@@ -26,6 +29,12 @@ public class PersonalFragment extends BaseFragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Event(R.id.l_wallet)
+    private void Wallet(View view){
+        Intent intent = new Intent(getActivity(), WalletActivity.class);
+        startActivity(intent);
     }
 
 }
